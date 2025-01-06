1bdb6ec16157c763c804ecc465b6050f9de52560
Rui Zheng <rzhengphy@gmail.com>
2025-01-07 00:55:19 +0800

    perf: add supabase config

---
+++ backend/app/config.py
@@ -0,0 +1,8 @@
+from os import environ
+
+from starlette.config import Config
+
+config = Config(environ.get("DOTENV_PATH", ".env"))
+
+SUPABASE_URL = config("SUPABASE_URL", cast=str)
+SUPABASE_KEY = config("SUPABASE_KEY", cast=str)
